340c66df94eff2d1ec9120a0e0b0fae4b1fe9442
Steven Smith <Steveice10@gmail.com>
2015-06-25 10:33:50 -0700

    Add cfg:nor support to ctrcommon and .3DS builds.

---
+++ include/ctrcommon/nor.hpp
@@ -0,0 +1,8 @@
+#ifndef __CTRCOMMON_NOR_HPP__
+#define __CTRCOMMON_NOR_HPP__
+
+#include "ctrcommon/types.hpp"
+
+bool norWrite(u32 offset, void* data, u32 size);
+
+#endif
---
+++ source/ctrcommon/nor.cpp
@@ -0,0 +1,19 @@
+#include "ctrcommon/nor.hpp"
+#include "ctrcommon/platform.hpp"
+
+#include "service.hpp"
+
+#include <3ds.h>
+
+bool norWrite(u32 offset, void* data, u32 size) {
+    if(!serviceRequire("nor")) {
+        return false;
+    }
+
+    Result result = CFGNOR_WriteData(offset, (u32*) data, size);
+    if(result != 0) {
+        platformSetError(serviceParseError((u32) result));
+    }
+
+    return result == 0;
+}
