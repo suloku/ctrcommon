fc61608125139227a3c3ed074198eb67b60e75ce
suloku <sulokutdcmago@gmail.com>
2015-09-07 19:46:42 +0200

    Updated GPU_DrawArray to latest CTRULIB
    Here's the change in the function:
    https://github.com/smealum/ctrulib/commit/11a5001f3321ce0deb62d79abfb62ac2ab54763a

--- source/ctrcommon/gpu.cpp
+++ source/ctrcommon/gpu.cpp
@@ -853,7 +853,7 @@ void gpuDrawVbo(u32 vbo) {
     if(vboData->indices != NULL) {
         GPU_DrawElements((GPU_Primitive_t) vboData->primitive, (u32*) vboData->indices, vboData->numVertices);
     } else {
-        GPU_DrawArray((GPU_Primitive_t) vboData->primitive, vboData->numVertices);
+        GPU_DrawArray((GPU_Primitive_t) vboData->primitive, 0, vboData->numVertices);
     }
 }
 
